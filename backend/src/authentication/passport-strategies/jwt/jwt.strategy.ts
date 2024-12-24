17f459a7af4cb2fafd0399c9e007332234dcd27c
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2024-12-25 05:04:21 +0800

    improved authentication jwt performance

--- backend/src/authentication/passport-strategies/jwt/jwt.strategy.ts
+++ backend/src/authentication/passport-strategies/jwt/jwt.strategy.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport"
 import { Strategy, ExtractJwt } from "passport-jwt"
 import { PrismaService } from "src/db/prisma/prisma.service";
@@ -12,4 +12,12 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
             secretOrKey: process.env.JWT_SECRET || "thequickbrownfox",
         });
     };
+    async validate(payload: JwtPayload) {
+        const user = await this.prisma.user.findUnique({ where: { id: payload.sub } });
+        if (!user) {
+            throw new UnauthorizedException('User not found');
+        }
+        const { createdAt, password, updatedAt, username, ...userObj } = user;
+        return userObj;
+    }
 }
