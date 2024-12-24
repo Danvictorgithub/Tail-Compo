0dc105e9bb0e2019affe24aebebc485f9d19cc1e
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2024-12-25 04:26:48 +0800

    updated schema and added logo

---
+++ backend/prisma/migrations/20241224181050_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `token` on the `emailToken` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "emailToken" DROP COLUMN "token";
---
+++ frontend/app/testing/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <img src="http://localhost:7200/public/tailchro.png" alt="" />
+    </div>
+  );
+}
