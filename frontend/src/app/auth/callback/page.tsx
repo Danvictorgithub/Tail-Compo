851a0b2c8276644d2ff0973f46015ae5b770778a
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-01-14 13:25:40 +0800

    added suspense on auth

--- frontend/src/app/auth/callback/page.tsx
+++ frontend/src/app/auth/callback/page.tsx
@@ -1,34 +1,36 @@
-"use client";
-import React, { useEffect } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
-import { signIn, useSession } from "next-auth/react";
+'use client';
+import React, { Suspense, useEffect } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { signIn, useSession } from 'next-auth/react';
 
 export default function Page() {
   const { status } = useSession();
   const router = useRouter();
-  const access_token = useSearchParams().get("access_token");
+  const access_token = useSearchParams().get('access_token');
 
   useEffect(() => {
-    if (status === "unauthenticated" && access_token) {
-      signIn("credentials", {
+    if (status === 'unauthenticated' && access_token) {
+      signIn('credentials', {
         access_token,
-        callbackUrl: "/",
+        callbackUrl: '/',
       });
-    } else if (status === "authenticated") {
-      router.push("/");
+    } else if (status === 'authenticated') {
+      router.push('/');
     }
   }, [status, access_token, router]);
 
   return (
-    <main className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="text-center">
-        <div className="flex justify-center items-center space-x-2">
-          <div className="w-4 h-4 rounded-full animate-ping bg-blue-500"></div>
-          <div className="w-4 h-4 rounded-full animate-ping bg-blue-500"></div>
-          <div className="w-4 h-4 rounded-full animate-ping bg-blue-500"></div>
+    <Suspense>
+      <main className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="text-center">
+          <div className="flex justify-center items-center space-x-2">
+            <div className="w-4 h-4 rounded-full animate-ping bg-blue-500"></div>
+            <div className="w-4 h-4 rounded-full animate-ping bg-blue-500"></div>
+            <div className="w-4 h-4 rounded-full animate-ping bg-blue-500"></div>
+          </div>
+          <p className="mt-4 text-lg text-gray-700">Logging in...</p>
         </div>
-        <p className="mt-4 text-lg text-gray-700">Logging in...</p>
-      </div>
-    </main>
+      </main>
+    </Suspense>
   );
 }
