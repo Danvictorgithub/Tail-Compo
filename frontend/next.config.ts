ba1ab1a6bf1bc1c6cbccc1d10450f6107702ff55
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2025-01-14 13:11:37 +0800

    updated nextjs config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,16 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
